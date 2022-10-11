34ee77a66ed36a5e3c824afa830741eda8d95f1b
Lhyerin <113421398+Lhyerin@users.noreply.github.com>
2022-10-11 17:01:17 +0900

    03

--- main.cpp
+++ main.cpp
@@ -3,16 +3,27 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 
-void func1(void){
-	int x;
-	printf("func1 x is at %p\n",&x);
-
+int sumTwo(int a, int b){
+	return(a+b);
+}
+int square(int n){
+	return(n*n);
+}
+int get_max(int x, int y){
+	if(x>y)
+	    return(x);
+	else
+	    return(y); 
 }
 
+
 int main(void){
-	int x;
-	printf("man x is at %p\n", &x);
-	func1();
+	int res_sum,res_square,res_get_max;
 	
+	res_sum=sumTwo(2,3);
+	res_square=square(5);
+	res_get_max=get_max(10,-20);
+	
+	printf("%i,%i,%i",res_sum,res_square,res_get_max);
 	return 0;
 }
