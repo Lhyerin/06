ab694c78a76875ee360fa7e922dd93d5b3c29677
Lhyerin <113421398+Lhyerin@users.noreply.github.com>
2022-10-11 17:06:22 +0900

    04

--- main.cpp
+++ main.cpp
@@ -2,28 +2,14 @@
 #include <stdio.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
-int sumTwo(int a, int b){
-	return(a+b);
-}
-int square(int n){
-	return(n*n);
-}
-int get_max(int x, int y){
-	if(x>y)
-	    return(x);
-	else
-	    return(y); 
+int square(int a)
+{
+	return(a*a);
 }
 
-
-int main(void){
-	int res_sum,res_square,res_get_max;
-	
-	res_sum=sumTwo(2,3);
-	res_square=square(5);
-	res_get_max=get_max(10,-20);
-	
-	printf("%i,%i,%i",res_sum,res_square,res_get_max);
-	return 0;
+int main()
+{
+	int a=2;
+	a=square(a);
+	printf("a=%i\n",a);
 }
